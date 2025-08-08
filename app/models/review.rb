417cd5bc619dee48eee6342a601ef5b99c9e3d87
Brian Guacarán <brianguacaran@gmail.com>
2025-08-08 03:07:15 -0400

    Improving review model validations & controller specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,8 +5,10 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates :rating, presence: true, inclusion: { in: 1..5 }
-  validates :comment, presence: true, length: { maximum: 1000 }
+  validates :rating, :comment, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5,
+                                    message: 'must be between 1 and 5' }
+  validates :comment, length: { maximum: 1000 }
 
   default_scope { order(created_at: :desc) }
   scope :from_active_users, -> { joins(:user).where(users: { status: :active }) }
