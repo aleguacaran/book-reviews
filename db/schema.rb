7e5a24436751a5c9094e5968b1a71c0fb1aa75d8
Brian Guacarán <brianguacaran@gmail.com>
2025-08-08 00:53:03 -0400

    Added scaffold for users module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_08_000648) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_043813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_08_000648) do
     t.string "title", null: false
     t.string "author", null: false
     t.date "published_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "status", default: "active", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
