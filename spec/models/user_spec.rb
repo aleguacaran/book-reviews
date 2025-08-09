7ab50caa4136d1e8779bd8765b951627dfd35116
Brian Guacarán <brianguacaran@gmail.com>
2025-08-08 21:22:52 -0400

    Improving books rating calculation when users are changed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:reviews).dependent(:destroy) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:status) }
@@ -10,6 +14,18 @@ RSpec.describe User, type: :model do
 
     it 'has a valid factory' do
       expect(build(:user)).to be_valid
+    end
+  end
+
+  describe 'after_save' do
+    let(:user) { create(:user) }
+    let(:book) { create(:book) }
+
+    before { create_list(:review, 3, user: user, book: book, rating: 4) }
+
+    it '#update_reviewed_books' do
+      user.banned!
+      expect { book.reload }.to change(book, :rating).from(4.0).to(nil)
     end
   end
 end
